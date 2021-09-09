02f0e14dec4592783b1de8b3b6828823f9510805
wied artha pratama <wiedarthapratama@gmail.com>
2021-09-09 13:06:02 +0700

    fix login

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:galon/widget/extra_screen/splash.dart';
 import 'package:get/get_navigation/get_navigation.dart';
-
-import 'extra_screen/splash.dart';
 
 void main() {
   runApp(MyApp());
---
+++ lib/widget/input_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InputWidget extends StatelessWidget {
+  final Widget? child;
+  const InputWidget({Key? key, @required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(30)),
+        child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20), child: child));
+  }
+}
